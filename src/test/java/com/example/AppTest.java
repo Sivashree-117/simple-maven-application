6d7cd5e2b6b08dee817d0fad57136584c6a60e8c
Sivashree-117 <sivashree.pl2023@vitstudent.ac.in>
2026-02-19 13:42:27 +0530

    Fix typo in App class instantiation in tests

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,9 +2,9 @@ package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class AppTest {
- @Test
- public void testAdd() {
- App app = new App();
- assertEquals(5, app.add(2, 3));
- }
+  @Test
+  public void testAdd() {
+    App app = new Appp();
+    assertEquals(5, app.add(2,3));
+  }
 }
